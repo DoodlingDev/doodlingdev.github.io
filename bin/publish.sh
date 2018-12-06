6bdf266b9c71d5fad6a4ef0920a3d624f82a8b87
DoodlingDev <aji.slater@gmail.com>
2018-12-06 11:37:44 -0800

    new content Thu Dec  6 11:37:44 PST 2018

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,26 +1,50 @@
 #!/usr/bin/env bash
 
+fancy_echo() {
+  local fmt="$1"; shift
+  printf "\n$fmt\n" "$0"
+}
+
+fancy_echo "beginning publish process..."
 cd ~/dev/doodlingdev.github.io
 
+fancy_echo "Trashing out-of-date build folder"
+trash _site
+
+fancy_echo "building..."
 jekyll build
 
+fancy_echo "recursively copying built site"
 cp -r _site ../temp
 
+fancy_echo "checking out master (published) branch"
 git checkout master
 
 if [ $? -ne 0 ]; then
-  echo "checkout to master failed. aborting."
-  exit
+  fancy_echo "master checkout failed. aborting"
 fi
 
-trash ./**
+fancy_echo "trashing out-of-date non-hidden files"
+trash *
 
-mv ../temp/_site/** .
+fancy_echo "copying updated site"
+cp -r ../temp/_site/** .
 
+if [ $? -ne 0 ]; then
+  fancy_echo "copying failed. aborting."
+fi
+
+fancy_echo "removing uneccessary _site/"
+trash ./_site
+
+fancy_echo "git adding folder"
 git add .
 
-git commit -m "new post $(date)"
+fancy_echo "git committing update"
+git commit -m "new content $(date)"
 
+fancy_echo "force-pushing to origin master"
 git push origin master --force
 
+fancy_echo "checking out dev branch"
 git checkout dev
